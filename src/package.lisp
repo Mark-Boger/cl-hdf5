90ce3bd2ba145b2dfa8956d48dfdabca91dfef9c
Mark Boger <93mar.bog@gmail.com>
2019-06-05 19:11:42 -0400

    Added symbols that should've been exported

--- src/package.lisp
+++ src/package.lisp
@@ -2,15 +2,52 @@
   (:use #:cl
         #:hdf5)
   (:export
-   ;; File functions
+   ;; Generic
+   #:hdf5-close
+
+   ;; Macros
+   #:with-hdf5
+   #:with-open-hdf5-file
+   #:with-open-hdf5-files
+   #:with-dataspace
+   #:with-datasets
+   #:with-dataset
+   
+   ;; Files
    #:is-hdf5-file
    #:create-hdf5-file
    #:open-hdf5-file
    #:close-hdf5-file
+   #:make-hdf5-file
 
-   ;; Dataspace functions
+   ;; Dataspaces
    #:create-dataspace
    #:close-dataspace
 
+   ;; Datasets
+   #:read-from-dataset
+   #:write-to-dataset
+   #:open-dataset
+   #:create-dataset
+   #:make-dataset
+
+   ;; Types
+   #:make-datatype
+   
    ;; Constants
-   #:+unlimited+))
+   #:+unlimited+
+   #:+default-properties+
+   #:+hdf5-types+
+
+   ;; Errors
+   #:hdf5-error   
+   #:hdf5-invalid-id
+   #:hdf5-close-error
+   #:hdf5-file-error
+   #:hdf5-file-creation-error
+   #:hdf5-file-open-error
+   #:dataset-creation-error
+   #:dataset-invalid-dataspace
+   #:dataspace-error
+   #:dataspace-size-mismatch
+   #:dataspace-creation-error))
